b17f8b4b210dad2eed61aaa889e806abfab468d4
Zhumagul01 <Jumagulrysbekkyzy@gmail.com>
2022-09-13 23:55:38 +1100

    'todo'

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Home from "./components/Home"
+
+// https://www.googleapis.com/youtube/v3/playlistItems
+
+function App() {
+  
+    return(
+  <>
+  <Home/>
+  </>
+    )
+    
+  
+}
+
+export default App;
